b2ff60fc1bd105d2e22ed53a643bfc28c0abf539
bernice mcbride <bernice mcbride>
2017-04-06 20:20:35 +0100

    links page

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
+Route::get('links', function () {
+    return view('links');
+});
+
 Route::get('about', function () {
     return view('about');
 });
