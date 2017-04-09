d70d856affca70e57d1b6320d567607931014575
Rachel Emsley <rccrowley86@googlemail.com>
2017-04-09 16:02:07 +0100

    create posts form

--- resources/views/Posts/create.blade.php
+++ resources/views/Posts/create.blade.php
@@ -11,6 +11,16 @@
         <h1>Create New Post</h1>
         <hr>
         
+       
+        <form action="/posts" method="post">
+
+  <input type="text" name="Title:" value=null>
+  <br>
+  <input type="text" name="Content" value=null>
+  <br><br>
+  <input type="submit" value="Submit">
+            
+        </form>
                 
 </div>
 </div>
@@ -21,6 +31,20 @@
 @section('title', '|Create New Post')
 
 @section('content')
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">     
+        <h1>Create New Post</h1>
+        <hr>
+{!! Form::open(['route' => 'posts.store']) !!}
+    {{Form::label('title', 'Title:')}}
+    {{Form::text('title', null, ['class' => 'form-control'])}}
+
+    {{Form::label('body', 'Content:')}}
+    {{Form::textarea('body', null, ['class' => 'form-control'])}}
+    {{Form::submit('Create Post', ['class' => 'btn btn-success btn-lg btn-block'])}}
+{!! Form::close() !!}
+</div>
+</div>
 @endsection-->
 
 
