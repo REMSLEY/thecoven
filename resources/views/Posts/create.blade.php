f2da8f716df977c7f9d36fec781cb28b4abb4894
MirenHall <mirenhall@gmail.com>
2017-04-09 14:26:40 +0100

    Started main.blade.php template file

---
+++ resources/views/Posts/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        @include('partials._head')
+    </head>
+    
+    <body>
+        @include('partials._nav')
+        
+        <div class="container"> 
+          @yield('content')
+          @include('partials._footer')
+        </div> <!-- end of the .container / partial is the name of something that get reused a lot-->  
+        
+          @include('partials._javascript') 
+          @yield('scripts')
+          
+    </body>
+</html>   
