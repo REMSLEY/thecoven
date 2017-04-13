b7cc488b5f40de0229bd4797850e587a1ecff7ce
Rachel Emsley <rccrowley86@googlemail.com>
2017-04-13 15:13:30 +0100

    seeder created

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'user_id' => rand(1,10),
+            'title' => str_random(30),
+            'body'=> str_random(300),
+            'created_at'=> new DateTime(),
+            'updated_at'=> new DateTime(),
+            'end_date'=> new DateTime(),
+            'update_id'=>rand(1,10)
+            
+            
+        ]);
+    }
+}
