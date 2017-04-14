42fda7d9a614061fcd77d56a8aff62809daa7d74
Rachel Emsley <rccrowley86@googlemail.com>
2017-04-14 13:19:30 +0100

    error messages added

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -3,3 +3,13 @@
 <strong>Success:</strong> {{Session::get('success')}}
 @endif
 
+@if (count($errors) > 0)
+<div class="alert alert-danger" role="alert">
+    <strong>Errors:</strong>
+  <ul>
+    @foreach ($errors->all() as $error)
+    <li>{{$error}}</li>
+  </ul>
+    @endforeach
+</div>
+@endif
