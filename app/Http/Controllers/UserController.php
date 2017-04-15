284a7d5472602e2563e0562cffc4b85e94861963
Rachel Emsley <rccrowley86@googlemail.com>
2017-04-15 22:40:28 +0100

    users.index created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UserController extends Controller
 {
@@ -14,7 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        //where returning a specific user by ID - only an admin should be able to see all users
+      
     }
 
     /**
