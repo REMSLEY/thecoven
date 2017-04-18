a093772a0c5af7640ce0fda63518f6fdffd912c1
Sally Bridgewater <sbridgewater24@gmail.com>
2017-04-18 23:11:59 +0100

    Messing with database settings again

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,18 +55,18 @@ return [
             //'host' => env('DB_HOST', 'localhost') . ('homestead' == gethostname() ? null : ':33060'),
         ],
 
-//        'pgsql' => [
-//            'driver' => 'pgsql',
-//            'host' => parse_url(getenv("DATABASE_URL"))["host"],
-//            'port' => env('DB_PORT', '5432'),
-//            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-//            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-//            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//            'schema' => 'public',
-//            'sslmode' => 'prefer',
-//        ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => getenv("DATABASE_URL")["host"],
+            'port' => env('DB_PORT', '5432'),
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => getenv("DATABASE_URL")["user"],
+            'password' => getenv("DATABASE_URL")["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
     ],
 
