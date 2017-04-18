1b1dfcf6e31753e6421011eb733a53e01b04acfd
Sally Bridgewater <sbridgewater24@gmail.com>
2017-04-18 23:18:10 +0100

    Messing with database settings again

--- config/database.php
+++ config/database.php
@@ -57,11 +57,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => getenv("DATABASE_URL")["host"],
+//            'host' => substr(parse_url(getenv("DATABASE_URL"))["host"], 1),
+            'host' => 'ec2-54-247-99-159.eu-west-1.compute.amazonaws.com',
             'port' => env('DB_PORT', '5432'),
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => getenv("DATABASE_URL")["user"],
-            'password' => getenv("DATABASE_URL")["pass"],
+//            'username' => getenv("DATABASE_URL")["user"],
+            'username' => 'hogkkospkvrxuz',
+//            'password' => getenv("DATABASE_URL")["pass"],
+            'password' => '13a021d612f83a3a194d1e33b2871b33358d552c0c22ad8eb473ce7b2cf192dd',
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
