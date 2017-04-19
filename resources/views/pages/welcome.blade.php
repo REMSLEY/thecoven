244eef28785e0f38a88cddfd3950624fc0064980
Rachel Emsley <rccrowley86@googlemail.com>
2017-04-19 16:39:33 +0100

    fix to text

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -30,7 +30,7 @@
   @foreach($topPosts as $post)
    <div class="post">
        <h3>{{$post->title}}</h3>
-       <p>{!! substr($post->body, 0, 300) !!}{{ strlen($post->body) > 300 ? '...' : '' }}</p>
+       <p>{{ substr(strip_tags($post->body), 0, 300) }}{{ strlen($post->body) > 300 ? '...' : '' }}</p>
        <a href='{{ url('blog/'.$post->slug) }}' class='btn btn primary'> Read More </a>
    
    </div>
